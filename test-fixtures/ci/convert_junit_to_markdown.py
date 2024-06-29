a71f6cd703e6e66bc15ed3c7a38a8dc7a3f4f97a
Clare So <cso@mozilla.com>
2024-06-28 23:20:34 -0400

    Use new arguments for Python code

--- test-fixtures/ci/convert_junit_to_markdown.py
+++ test-fixtures/ci/convert_junit_to_markdown.py
@@ -56,67 +56,46 @@ def count_test_retry_failure(test_name, test_cases):
             count += 1
     return count
 
-def convert_to_github_markdown(test_suites):
-    markdown = "# Test Results\n\n"
-
-    for test_suite in test_suites:
-        markdown += "## {name}\n\n".format(name = test_suite.get('name', '').replace('XCUITest.' ,''))
-        markdown += "* Number of tests: {tests}\n".format(tests = test_suite['tests'])
-        markdown += "* Number of failures: {failures}\n\n".format(failures = test_suite['failures'])
-        markdown += convert_test_cases_to_github_markdown(test_suite['test_cases'])
-    return markdown
-
-def convert_to_slack_markdown(test_suites):
+def convert_to_slack_markdown(test_suites, is_smoke = True):
     markdown = ""
     for test_suite in test_suites:
         if int(test_suite.get('failures')):
             markdown += "{test_suite_name}\\n".format(test_suite_name = test_suite.get('name', '').replace('XCUITest.' ,''))
             test_cases = test_suite.get('test_cases', [])
             done = []
+            
             for test_case in test_cases:
-                if not test_case.get('status') == '✅' and not test_case.get('name','') in done:
-                    fail_count = count_test_retry_failure(test_case.get('name', ''), test_cases)
-                    # flaky test?
-                    if fail_count < 3:
-                        test_case['status'] = ':warning:'
+                if not test_case.get('status') == '✅' and not test_case.get('name','') in done:        
+                    # For smoke test only: See if the test passes in 2nd or 3rd attempt
+                    if is_smoke:
+                        fail_count = count_test_retry_failure(test_case.get('name', ''), test_cases)
+                        if fail_count < 3:
+                            test_case['status'] = ':warning:'
+                    
                     markdown += "{status} `{test_case_name}`\\n".format(test_case_name=test_case.get("name"), status=test_case.get('status'))
                     done.append(test_case.get('name', ''))
+
     if markdown == "":
         markdown += "🎉 No test failures 🎉"
 
     return markdown    
 
-def convert_to_github_markdown_failures_only(test_suites):
+def convert_to_github_markdown(test_suites, is_smoke = True):
     markdown = ""
+    
     for test_suite in test_suites:
         if int(test_suite['failures']):
             markdown += "## {name}\n\n".format(name = test_suite.get('name', '').replace('XCUITest.' ,''))
-            markdown += convert_test_cases_to_github_markdown_failures_only(test_suite.get('test_cases', []))
+            markdown += convert_test_cases_to_github_markdown(test_suite.get('test_cases', []), is_smoke = True)
+    
     if markdown == "":
         markdown += "## 🎉 No test failures 🎉"
     else:
-        markdown += "\n\n⚠️-Flaky ❌-Failed"
-    return markdown
-
-def convert_test_cases_to_github_markdown(test_cases):
-    markdown = ""
-    markdown += "| Test Name | Time (s) | Status | Message |\n"
-    markdown += "|-----------|----------|--------|---------|\n"
-
-    for test_case in test_cases:
-        message = test_case.get('message', '')
-        message = ('```{message}```'.format(message = message) if message != '' else '')
-        markdown += "| {name} | {time} | {status} | {message} |\n".format(
-            name = test_case.get('name', ''),
-            time = test_case.get('time', ''),
-            status = test_case.get('status'),
-            message = message
-        )
-    markdown += "\n"
+        markdown = "(:warning:-flaky :x:-Failed)\n" + markdown
     
     return markdown
 
-def convert_test_cases_to_github_markdown_failures_only(test_cases):
+def convert_test_cases_to_github_markdown(test_cases, is_smoke = True):
     markdown = ""
     markdown += "| Test Name | Status | Message |\n"
     markdown += "|-----------|--------|---------|\n"
@@ -124,11 +103,13 @@ def convert_test_cases_to_github_markdown_failures_only(test_cases):
     done = []
     
     for test_case in test_cases:
-        if not test_case.get('status') == '✅' and not test_case.get('name', '') in done:
-            fail_count = count_test_retry_failure(test_case.get('name'), test_cases)
-            # flaky test?
-            if fail_count < 3:
-                test_case['status'] = '⚠️'
+        if not test_case.get('status') == '✅' and not test_case.get('name', '') in done:   
+            # For smoke test only: See if the test passes in 2nd or 3rd attempt
+            if is_smoke:
+                fail_count = count_test_retry_failure(test_case.get('name'), test_cases)
+                if fail_count < 3:
+                    test_case['status'] = '⚠️'
+            
             message = test_case.get('message', '')
             message = message = ('```{message}```'.format(message = message) if message != '' else '')
             markdown += "| {name} | {status} | {message} |".format(
@@ -136,37 +117,37 @@ def convert_test_cases_to_github_markdown_failures_only(test_cases):
                 status = test_case.get('status'),
                 message = message
             )
+            markdown += "\n"   
             done.append(test_case.get('name'))
-            markdown += "\n"
     
     return markdown
 
-def convert_file_github(input_file, output_file, failures_only = False):
+def convert_file_github(input_file, output_file, is_smoke = True):
     test_cases = parse_junit_xml(input_file)
-    markdown = ""
-    if failures_only:
-        markdown = convert_to_github_markdown_failures_only(test_cases)
-    else:
-        markdown = convert_to_github_markdown(test_cases)
+    markdown = convert_to_github_markdown(test_cases, is_smoke = True)
     with open(output_file, 'w') as md_file:
         md_file.write(markdown)
 
-def convert_file_slack(input_file, output_file):
+def convert_file_slack(input_file, output_file, is_smoke = True):
     test_cases = parse_junit_xml(input_file)
-    markdown = convert_to_slack_markdown(test_cases)
+    markdown = convert_to_slack_markdown(test_cases, is_smoke = True)
     with open(output_file, 'w') as md_file:
         md_file.write(markdown)
 
 if __name__ == "__main__":
-    opts, args = getopt.getopt(sys.argv[1:], "fgs", ["failures-only", "github", "slack"])
+    opts, args = getopt.getopt(sys.argv[1:], "", ["github", "slack", "smoke", "full-functional"])
+    
     failures_only = False
     github_markdown = True
+    is_smoke = True
+    
     for opt, arg in opts:
-        if opt == "-f" or opt == "--failures-only":
-            failures_only = True
-        if opt == "-s" or opt == "--slack":
+        if opt == "--slack":
             github_markdown = False
+        if opt == "--full-functional":
+            is_smoke = False
+    
     if github_markdown:
-        convert_file_github(args[0], args[1], failures_only=failures_only)
+        convert_file_github(args[0], args[1], is_smoke=is_smoke)
     else:
-        convert_file_slack(args[0], args[1])
+        convert_file_slack(args[0], args[1], is_smoke=is_smoke)
